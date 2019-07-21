8b21bd7289d24af2f568dca0ba2555f56f46aee9
Penguin-71630 <53132291+Penguin-71630@users.noreply.github.com>
2019-07-21 13:51:30 +0800

    Update and rename Unlock Pattern.py to Kattis/Unlock Pattern.py

--- Kattis/Unlock Pattern.py
+++ Kattis/Unlock Pattern.py
@@ -1,7 +1,3 @@
-import logging as l
-
-l.basicConfig(filename='unlock pattern', level=l.INFO)
-
 pattern = []
 for i in range(3):
     line = [int(j) for j in input().split()]
@@ -25,10 +21,7 @@ last_index = find_index(1)
 distance = 0
 
 for i in range(2, 10):
-    l.info('i:{}'.format(i))
     distance += find_distance(find_index(i))
     last_index = find_index(i)
-    l.info('distance:{}'.format(distance))
-    l.info('last_index:{}'.format(last_index))
 
 print(distance)
